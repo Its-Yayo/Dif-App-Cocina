81b1e8dfc3b88dc98b663a8eff6e95f6545d14c5
Wuashinango <juancarloscarro_@hotmail.com>
2023-10-12 13:44:20 -0600

    correcion de la pantalla del scanner

--- mlkitscan/app/src/main/java/com/equipo2/mlkitscan/MainActivity.kt
+++ mlkitscan/app/src/main/java/com/equipo2/mlkitscan/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
@@ -20,27 +18,8 @@ class MainActivity : AppCompatActivity() {
         val btnIniciar = findViewById<Button>(R.id.iniciarApp)
 
         btnIniciar.setOnClickListener{
-            val options = GmsBarcodeScannerOptions.Builder()
-                .setBarcodeFormats(
-                    Barcode.FORMAT_QR_CODE
-                )
-                .enableAutoZoom()
-                .build()
-            val scanner = GmsBarcodeScanning.getClient(this)
-            scanner.startScan()
-                .addOnSuccessListener { barcode ->
-                    // Tarea completada con éxito
-                    val barcodeValue = barcode.rawValue.toString()
                     val intent = Intent(this, ReinicioActivity::class.java)
-                    intent.putExtra("barcodeValue", barcodeValue)
                     startActivity(intent)
-                }
-                .addOnCanceledListener {
-                    // Tarea cancelada
-                }
-                .addOnFailureListener { e ->
-                    // Tarea fallida con una excepción
-                }
         }
     }
 }
--- mlkitscan/app/src/main/java/com/equipo2/mlkitscan/Reinicio.kt
+++ mlkitscan/app/src/main/java/com/equipo2/mlkitscan/Reinicio.kt
@@ -5,20 +5,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 
 class ReinicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pantalla_de_reinicio)
 
-        val barcodeValue = intent.getStringExtra("barcodeValue")
-        val textView = findViewById<TextView>(R.id.textoCodigo)
-        textView.text = "Barcode value: $barcodeValue"
+        // Inicializa el escáner
+        val options = GmsBarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE
+            )
+            .enableAutoZoom()
+            .build()
 
-        val restartButton = findViewById<Button>(R.id.btnReiniciar)
-        restartButton.setOnClickListener {
-            finish()
-            startActivity(Intent(this, MainActivity::class.java))
-        }
+        val scanner = GmsBarcodeScanning.getClient(this)
+        scanner.startScan()
+            .addOnSuccessListener { barcode ->
+                // Tarea completada con éxito
+                val barcodeValue = barcode.rawValue.toString()
+
+                // Configura la vista después del escaneo
+                setContentView(R.layout.pantalla_de_reinicio)
+
+                val textView = findViewById<TextView>(R.id.textoCodigo)
+                textView.text = "Barcode value: $barcodeValue"
+
+                val restartButton = findViewById<Button>(R.id.btnReiniciar)
+                restartButton.setOnClickListener {
+                    finish()
+                    startActivity(Intent(this, ReinicioActivity::class.java))
+                }
+            }
+            .addOnCanceledListener {
+                // Tarea cancelada
+            }
+            .addOnFailureListener { e ->
+                // Tarea fallida con una excepción
+            }
     }
 }
