d8e84202136912c8c6d92cf9f16a072f49300e48
Wuashinango <juancarloscarro_@hotmail.com>
2023-10-15 17:40:43 -0600

    agregarPNG

--- aplicacionFinal/app/build.gradle.kts
+++ aplicacionFinal/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.equipo2.aplicacionfinal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.equipo2.aplicacionfinal"
@@ -41,6 +41,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    //AQUI INICIAR MIS IMPLEMENTACIONES
+    //Scanner QR de google
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- aplicacionFinal/app/src/main/java/com/equipo2/aplicacionfinal/MainActivity.kt
+++ aplicacionFinal/app/src/main/java/com/equipo2/aplicacionfinal/MainActivity.kt
@@ -2,10 +2,20 @@ package com.equipo2.aplicacionfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Accede a los TextInputEditText usando sus IDs
+        val emailEditText: TextInputEditText = findViewById(R.id.usuario)
+        val contrasenaEditText: TextInputEditText = findViewById(R.id.Contrasena)
+
+        // Ahora puedes usar emailEditText y contrasenaEditText en tu código
+        // por ejemplo, para obtener el texto:
+
+        val emailText = emailEditText.text.toString()
+        val contrasenaText = contrasenaEditText.text.toString()
     }
 }
