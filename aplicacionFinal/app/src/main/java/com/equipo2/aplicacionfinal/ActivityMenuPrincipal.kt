25e086ad6c4936270e87834c7487f8134ba735aa
Wuashinango <juancarloscarro_@hotmail.com>
2023-10-18 14:38:09 -0600

    todo nuevo

--- aplicacionFinal/app/src/main/java/com/equipo2/aplicacionfinal/ActivityMenuPrincipal.kt
+++ aplicacionFinal/app/src/main/java/com/equipo2/aplicacionfinal/ActivityMenuPrincipal.kt
@@ -1,11 +1,13 @@
 package com.equipo2.aplicacionfinal
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
 
 class ActivityMenuPrincipal : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
@@ -13,6 +15,8 @@ class ActivityMenuPrincipal : AppCompatActivity() {
         val cardScanner = findViewById<CardView>(R.id.card_qr)
         val cardScannerAyudantes = findViewById<CardView>(R.id.card_qrayudantes)
         val cardReportes = findViewById<CardView>(R.id.card_reportar)
+        val cardLogOut =findViewById<CardView>(R.id.card_logout)
+        val cardInventario = findViewById<CardView>(R.id.card_inventario)
 
         cardScanner.setOnClickListener{
             val intent = Intent(this, ActivityScanner::class.java)
@@ -26,6 +30,13 @@ class ActivityMenuPrincipal : AppCompatActivity() {
             val intent = Intent(this, ActivityReport::class.java)
             startActivity(intent)
         }
-
+        cardLogOut.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        cardInventario.setOnClickListener{
+            val intent = Intent(this, ActivityInventario::class.java)
+            startActivity(intent)
+        }
     }
 }
