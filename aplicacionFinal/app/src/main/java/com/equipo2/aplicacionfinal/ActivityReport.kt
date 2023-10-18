9b2750c92a0d6168f02905fa9c7303c7c594fc16
Wuashinango <juancarloscarro_@hotmail.com>
2023-10-18 09:16:54 -0600

    funcion de los dos botones

---
+++ aplicacionFinal/app/src/main/java/com/equipo2/aplicacionfinal/ActivityReport.kt
@@ -0,0 +1,11 @@
+package com.equipo2.aplicacionfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityReport : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
